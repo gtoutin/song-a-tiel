2826363d83444363ede1258f4f7161989de2dbcb
gtoutin <gctoutin@gmail.com>
2022-06-24 22:48:13 -0500

    HTML Pages and working song page
    Bring it on!

--- SongaTiel/app.py
+++ SongaTiel/app.py
@@ -3,26 +3,44 @@ Flask app
 """
 
 
-from flask import Flask, request, Response
-from SongaTiel import SongaTiel
+from flask import Flask, request, Response, render_template
 import json
+
+from SongaTiel import SongaTiel
+from infotype import Type
 
 
 app = Flask(__name__)   # Flask app
 st = SongaTiel()        # SongaTiel object
 
-@app.route('/')
+
+@app.route('/', methods=['GET'])
+def homepage():
+    return render_template('index.html')
+
+
+@app.route('/results/')
 def info():
     # Grab the query params from the request.args dict
     song, album, artist = request.args.get("song",""), request.args.get("album",""), request.args.get("artist","")
 
     # Run the search
     st_results = st.search(song=song, album=album, artist=artist)
+    st_data = st_results['data']
     
-    # Make and return the search results
-    resp = Response(json.dumps(st_results))
-    resp.content_type = "application/json"
-    return resp
+    # # Make and return the search results
+    # resp = Response(json.dumps(st_results))
+    # resp.content_type = "application/json"
+
+    if st_results['type'] == Type.SONG.name:
+        return render_template('song.html',
+            song=st_data['name'],
+            artist=st_data['artist'],
+            album=st_data['album'],
+            release_date=st_data['release_date'],
+            lyrics=st_data['lyrics'],
+            length=st_data['length'],
+        )
 
 
 
