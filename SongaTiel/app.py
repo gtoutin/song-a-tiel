ff7edbb645dcf4eda8ea50a379dbefe1afa6a1a6
gtoutin <gctoutin@gmail.com>
2022-06-26 18:07:33 -0500

    PORT is an int

--- SongaTiel/app.py
+++ SongaTiel/app.py
@@ -65,4 +65,4 @@ def results():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
